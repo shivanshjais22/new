cb114b69516d2da23a21aa78013391a08e62773b
shivanshjais22 <jaisshivansh85@gmail.com>
2025-06-11 23:54:53 +0530

    this is just A DTO layer

---
+++ demo-2/src/main/java/dto/GenericProductDTO.java
@@ -0,0 +1,51 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GenericProductDTO {
+    private String id;
+    private String title;
+    private String category;
+    private double price;
+    private String description;
+    public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	private String image;
+}
