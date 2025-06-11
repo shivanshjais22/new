384db44e460f982e458687fa70a7ccb167b18824
shivanshjais22 <jaisshivansh85@gmail.com>
2025-06-11 23:35:56 +0530

    to scan all the package out side demo package we use scan class before run

--- demo-2/src/main/java/com/example/demo/Demo2Application.java
+++ demo-2/src/main/java/com/example/demo/Demo2Application.java
@@ -2,10 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "controller",
+    "dto",
+    "model",
+    "repository",
+    "service"
+})
 public class Demo2Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
